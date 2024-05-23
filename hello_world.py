30eb9c1d86a14c4c866600ca410e909d342074e6
MoneebaMeer <165048606+MoneebaMeer@users.noreply.github.com>
2024-05-23 20:28:51 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# Get user input: name and age.
+# Print user input with the greeting: Hello World!
+
+name = input("Enter your name: ")
+print(name)
+
+age = input("Enter your age: ")
+print(age)
+
+print("Hello world!")
